90d6ab627ee0d5620bde87625f232b990679d136
Jack Dean Watson-Hamblin <jack.hamblin@generalassemb.ly>
2015-01-20 15:42:32 +1100

    Example markdown usage

---
+++ README.md
@@ -0,0 +1,11 @@
+# Markdown with Redcarpet
+
+```ruby
+gem 'redcarpet'
+```
+
+Or install for use in PRY...
+
+```console
+gem install redcarpet
+```
---
+++ markdown_example_rails_app/app/helpers/application_helper.rb
@@ -0,0 +1,5 @@
+module ApplicationHelper
+  def markdown(input)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true).render(input).html_safe
+  end
+end
